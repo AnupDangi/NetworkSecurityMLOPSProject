700c880e2a6714d556c0195ff65ebdd9e3b0fb51
Anup Dangi <anupdangi1589@gmail.com>
2025-12-28 00:32:19 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 from networksecurity.logging.logger import logging
+from networksecurity.components.validation_config import DataValidation
 
 if __name__=="__main__":
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
-        data_ingestion=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(data_ingestion_config=data_ingestion)
+        data_ingestion_config=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
         logging.info("Exporting collection data as dataframe")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(data_ingestion_artifact=dataingestionartifact,data_validation_config=data_validation_config)
+        logging.info("Starting data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        logging.info("Data validation completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -31,3 +31,14 @@ class ModelTrainerArtifact:
     trained_model_file_path: str
     train_metric_artifact: ClassificationMetricArtifact
     test_metric_artifact: ClassificationMetricArtifact
+
+
+@dataclass 
+class DataValidationArtifact:
+    validation_status:bool
+    valid_train_file_path:str
+    valid_test_file_path:str
+    invalid_train_file_path:str
+    invalid_test_file_path:str
+    drift_report_file_path:str
+
