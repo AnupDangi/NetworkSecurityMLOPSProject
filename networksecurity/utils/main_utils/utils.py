fixed the silent killer that ie another projectzip was used in the code which doesnot exist replaced with current root folder files

networksecurity/utils/main_utils/utils.py
@@ -4,7 +4,7 @@ import os,sys
 import shutil
 import numpy as np
 import pickle
-import networksecurity.logging.logger as logger
+import networksecurity.logging.logger as logging
 import sys
 from sklearn.model_selection import GridSearchCV
 from  sklearn.metrics import r2_score
@@ -46,6 +46,18 @@ def save_numpy_array(file_path:str,array:np.array):
     
     except Exception as e:
         raise NetworkSecurityException(e,sys) from e
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+save_numpy_array_data = save_numpy_array
     
 
 def load_object(file_path:str)->object: